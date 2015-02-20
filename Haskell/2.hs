20d9c1fa7f6a8a3337afa73814a667f079402b1a
Emil Kloeden <emilkloeden@gmail.com>
2015-02-20 19:10:09 +1030

    Tidy up of comments in 2.hs only

--- Haskell/2.hs
+++ Haskell/2.hs
@@ -4,12 +4,18 @@ maxNum = 4000000
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)-- took from interwebs
 ans = [x | x <- takeWhile (<=maxNum) fibs,  even x] -- also took from interwebs
 
--- zipWith  - takes an operation (addition here)
---          - two lists and combines list1[i] with list2[i]
---          - using chosen operation
+{-
+    zipWith     - takes an operation (addition here)
+                two lists and combines list1[i] with list2[i]
+                using chosen operation
 
--- takeWhile - takes items from a list where predicate
---           - returns true takeWhile (predicate) list
+    takeWhile   - takes items from a list where predicate
+                returns true takeWhile (predicate) list
 
--- even     - takes a parameter and returns true is parameter
---          - is even.
+    even        - takes a parameter and returns true is 
+                parameter is even.
+    
+    print       - takes one argument (hence brackets)
+
+    sum         - sums a list
+-}
